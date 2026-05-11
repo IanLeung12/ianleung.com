42f24680fd30b1edff4c3d93822e8814de8871be
toxicface <47838537+toxicface@users.noreply.github.com>
2026-05-11 02:58:58 -0400

    changed buttons, added project skeleton

---
+++ src/data/project.ts
@@ -0,0 +1,11 @@
+export type Project = {
+    title: string;
+    short_desc: string;
+    description: string;
+    link?: string;
+    github?: string;
+    tools: string[];
+    thumbnail: string;
+    image?: string;
+    video?: string;
+}
