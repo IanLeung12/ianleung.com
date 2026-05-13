1cad92900b01262f61d8f64439ab17cb4d7b676d
toxicface <47838537+toxicface@users.noreply.github.com>
2026-05-13 17:05:29 -0400

    improved project cards

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,5 +10,23 @@ export const projects: Project[] = [
         tools: ["Android Studio", "Kotlin", "Openrouter", "Figma", "JavaMail"],
         thumbnail: "/images/pharmagotchi.png",
         image: "/images/pharmagotchi.png",
+    },
+    {
+        title: "Trust No Ghost",
+        short_desc: "3D Maze Exploration Game",
+        description: "There are several ghosts in this maze. One is friendly, the rest are not. You must figure out which one is which using their mixed signals.  Find the axe to kill the evil ones, then find the candy and give it to the good one to win! Don't get them mixed up though...",
+        github: "https://github.com/IanLeung12/Trust-No-Ghost",
+        tools: ["Unity", "C#", "Itch.io"],
+        thumbnail: "/images/trustnoghost.png",
+        image: "/images/trustnoghost.png",
+    },
+    {
+        title: "more",
+        short_desc: "will add later",
+        description: "temp",
+        github: "https://github.com/IanLeung12/IanLeung12",
+        tools: ["a"],
+        thumbnail: "",
+        image: "",
     }
 ]
