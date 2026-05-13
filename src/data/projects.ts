8b8b70434030b58eba797abfd717f362d3e6d8ce
toxicface <47838537+toxicface@users.noreply.github.com>
2026-05-13 03:57:56 -0400

    changed tool

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,7 @@ export const projects: Project[] = [
         short_desc: "Your virtual pharmacy friend.",
         description: "Download Pharmagotchi and adopt your own pharmacy friend! Your pharmagotchi provides regular reminders and health recommendations to keep you on top of your health. Graph your health, receive AI-powered feedback, and automatically connect with health care providers when needed!",
         github: "https://github.com/dilhrm/Pharmagotchi",
-        tools: ["Android Studio", "Kotlin", "Openrouter", "Figma", "XML"],
+        tools: ["Android Studio", "Kotlin", "Openrouter", "Figma", "JavaMail"],
         thumbnail: "/images/pharmagotchi.png",
         image: "/images/pharmagotchi.png",
     }
