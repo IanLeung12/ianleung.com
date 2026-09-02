243d58a1dbee07cf857733a85289dd1276a42774
toxicface <47838537+toxicface@users.noreply.github.com>
2026-09-02 04:04:17 -0400

    update about me and project descriptions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
     {
         title: "Pharmagotchi",
         short_desc: "Your virtual pharmacy friend.",
-        description: "Download Pharmagotchi and adopt your own pharmacy friend! Your pharmagotchi provides regular reminders and health recommendations to keep you on top of your health. Graph your health, receive AI-powered feedback, and automatically connect with health care providers when needed!",
+        description: "Tamagotchi-inspired Android app where your virtual pet (pharmagotchi) provides regular reminders and health recommendations to keep you on top of your health. Graph your metrics, receive AI-powered feedback, and automatically connect with health care providers when needed.",
         github: "https://github.com/dilhrm/Pharmagotchi",
         tools: ["Android Studio", "Kotlin", "Openrouter", "Figma", "JavaMail"],
         thumbnail: "/images/pharmagotchi.png",
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     {
         title: "Trust No Ghost",
         short_desc: "3D Maze Exploration Game",
-        description: "There are several ghosts in this maze. One is friendly, the rest are not. You must figure out which one is which using their mixed signals.  Find the axe to kill the evil ones, then find the candy and give it to the good one to win! Don't get them mixed up though...",
+        description: "Maze exploration game built in Unity where your objective is to find and feed a friendly ghost among several other evil ones. Includes custom animations, pathfinding mechanics, effects, and more.",
         github: "https://github.com/IanLeung12/Trust-No-Ghost",
         tools: ["Unity", "C#", "Itch.io"],
         thumbnail: "/images/trustnoghost.png",
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     {
         title: "PeerAssist",
         short_desc: "Peer Assistance Platform for Students",
-        description: "PeerAssist is a desktop platform for students to publish, peer-edit, and review each other's assignments and work. Users sign up, upload PDF documents tagged by grade level and subject, and leave marks and comments on other students' work. Documents can be searched, sorted, and ranked by average review score.",
+        description: "Java-based desktop app for students to publish, peer-edit, and review each other's assignments and work. Users sign up, upload PDF documents tagged by grade level and subject, and leave marks and comments on other students' work.",
         github: "https://github.com/IanLeung12/PeerAssist",
         tools: ["Java", "Supabase", "PDFBox", "Gson", "FlatLaf", "Maven build"],
         thumbnail: "/images/peerassist.png",
